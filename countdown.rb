f85d7d57e500715dce9a309b51c59fc0d8369290
William Stevens <wstevens.hornet@gmail.com>
2019-10-12 01:30:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(seconds)
-  while seconds 
-  puts "#{number} SECOND(S)!"
-  seconds -= 1 
-end
-"HAPPY NEW YEAR!"
+  while seconds > 0
+    puts "#{number} SECOND(S)!"
+    seconds -= 1 
+  end
+  "HAPPY NEW YEAR!"
 end
